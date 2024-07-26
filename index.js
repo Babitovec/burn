3da8eb5b971b6d2b72e309d176ad1875daac4cad
Babitovec <52136738+Babitovec@users.noreply.github.com>
2024-07-26 21:17:59 +0400

    index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const tg = window.Telegram.WebApp //позволяет работать с тг апи
 
-tg.expand(); //растягивает на фулл скрин при запуске
-tg.disableVerticalSwipes() //Отключает скролл
+document.addEventListener('DOMContentLoaded', function () {
+    const tg = window.Telegram.WebApp; // позволяет работать с тг апи
+    tg.expand(); // растягивает на фулл скрин при запуске
+    tg.disableVerticalSwipes(); // Отключает скролл
+});
