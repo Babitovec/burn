b67e06f11dd273e0cc7dde65d20a3df530c2a45d
Babitovec <52136738+Babitovec@users.noreply.github.com>
2024-07-29 01:49:02 +0400

    Add files via upload

---
+++ images/index.js
@@ -0,0 +1,15 @@
+const tg = window.Telegram.WebApp //позволяет работать с тг апи
+
+tg.expand(); // растягивает на фулл скрин при запуске
+tg.disableVerticalSwipes(); // Отключает скролл
+
+// Получаем имя пользователя Telegram
+const username = tg.initDataUnsafe.user ? tg.initDataUnsafe.user.username : "Unknown User";
+
+// Заменяем текст "Babitovec" на имя пользователя
+document.addEventListener("DOMContentLoaded", function() {
+    const usernameElements = document.getElementsByClassName("username");
+    for (let i = 0; i < usernameElements.length; i++) {
+        usernameElements[i].textContent = username;
+    }
+});
